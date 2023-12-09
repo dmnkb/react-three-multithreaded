6bd5cfa80d05e55be074e35832b8f14ee4a56aa9
Dominik <dev@borchert.me>
2023-12-09 16:10:16 +0100

    refactor

--- src/scene/terrain/useRemoteNoise/useRemoteNoise.ts
+++ src/scene/terrain/useRemoteNoise/useRemoteNoise.ts
@@ -5,7 +5,7 @@ export const useRemoteNoise = () => {
   const createRemoteNoise = (onNoiseCreated: (data: Int8Array) => void, offset: Vector3, chunkScale = 32) => {
     const worker = new Worker(new URL('./noiseWorker.js', import.meta.url));
 
-    // Make TypeScript believe this is callable.
+    // Make TypeScript believe this is callable
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const remoteFunction: any = Comlink.wrap<Worker>(worker);
 
